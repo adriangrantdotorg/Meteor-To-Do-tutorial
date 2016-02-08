94724842d9008de004ff97fc398532d92c13f533
adriangrantdotorg <adrian.agrant@gmail.com>
2016-02-08 01:03:02 -0500

    Completed Part 8

--- simple-todos.js
+++ simple-todos.js
@@ -20,13 +20,34 @@ if (Meteor.isClient) {
   
       // Clear form
       event.target.text.value = "";
+    },
+    
+    // Check if Completed tasks should be hidden 
+    "change .hide-completed input": function (event) {
+      Session.set("hideCompleted", event.target.checked);
     }
   });
 
   Template.body.helpers({
     tasks: function () {
-      // Show newest tasks at the top
-      return Tasks.find({}, {sort: {createdAt: -1}});    }
+      //Display & Hides Filtered tasks  
+        if (Session.get("hideCompleted")) {
+          // If hide completed is checked, filter tasks
+          return Tasks.find({checked: {$ne: true}}, {sort: {createdAt: -1}});
+        } else {
+          // Otherwise, return all of the tasks
+          return Tasks.find({}, {sort: {createdAt: -1}});
+        }
+      },
+      //maybe unnessary https://github.com/meteor/simple-todos/commit/3352071c6d1c414f94ee21038f28c45651d6216c#commitcomment-13403535
+      hideCompleted: function () {
+        return Session.get("hideCompleted");
+      },
+      
+      incompleteCount: function () {
+        return Tasks.find({checked: {$ne: true}}).count();
+      }
+
   });
 
   Template.task.events({
